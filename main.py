5617f548fa42acb5c1478b0d6070824bdbf18556
Anup-c4 <anupy696@gmail.com>
2026-01-08 22:57:31 +0530

    added schema

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
+
 app=FastAPI()
 @app.get('/')
 def index():
-    return {'message':'Welcome to Inventory Management App'}
+    return {'message':'Welcome to Inventory Management App. Use /docs for API Documentation'}
+
+register_tortoise(
+    app,
+    db_url='sqlite://database.sqlite3',
+    modules={"models":["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
--- models.py
+++ models.py
@@ -1,12 +1,25 @@
 from tortoise.models import Model
 from tortoise import fields
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 class Products(Model):
     id=fields.IntField(pk=True)
     name=fields.CharField(max_length=30, nullable=False)
     quantity_in_stock=fields.IntField(default=0)
     quantity_sold=fields.IntField(default=0)
-    unit_price=fields.FloatField(default=0.0,max_digits=8, decimal_places=2)  
+    unit_price=fields.FloatField(default=0.00,max_digits=8, decimal_places=2)  
     supplied_by=fields.ForeignKeyField('models.Suppliers', related_name='goods_supplied', on_delete=fields.CASCADE) 
-    revenue=fields.FloatField(default=0.0, max_digits=20, decimal_places=2) 
+    revenue=fields.FloatField(default=0.00, max_digits=20, decimal_places=2) 
     
+class Suppliers(Model):
+    id=fields.IntField(pk=True)
+    name=fields.CharField(max_length=20)
+    company=fields.CharField(max_length=20)
+    email=fields.CharField(max_length=50)
+    phone=fields.CharField(max_length=15)
+
+product_pydantic=pydantic_model_creator(Products, name="Product")
+product_pydanticIn=pydantic_model_creator(Products, name="ProductIn", exclude_readonly=True)
+supplier_pydantic=pydantic_model_creator(Suppliers, name="Supplier")
+supplier_pydanticIn=pydantic_model_creator(Suppliers, name="SupplierIn", exclude_readonly=True)
+
